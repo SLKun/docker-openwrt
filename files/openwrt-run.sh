1515142b0fa9cbacb50580d16af50252f96455f0
SLKun <summerslyb@gmail.com>
2020-08-21 01:11:44 +0800

    refine network

--- files/openwrt-run.sh
+++ files/openwrt-run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 function _usage() {
   echo "Could not find config file."
@@ -7,10 +7,12 @@ function _usage() {
 }
 
 function _cleanup() {
-  kill wpa_supplicant
-  docker stop $CONTAINER >/dev/null
+  # recovery route
+  sudo ip route add 192.168.100.0/24 dev enin0
+  sudo ip route del 192.168.100.100 dev enin0
+  # stop docker and netns
+  sudo docker stop $CONTAINER >/dev/null
   sudo rm -rf /var/run/netns/$CONTAINER
-  sudo ip link del dev ${LAN_IFACE}.wan
 }
 
 # detect config file
@@ -19,16 +21,15 @@ CONFIG_FILE=${1:-$SCRIPT_DIR/../openwrt.conf}
 source $CONFIG_FILE 2>/dev/null || { _usage; exit 1; }
 
 # connect WiFi
-WWAN_ON=$(ip link | grep ${WWAN_IFACE})
-if [[ -n "$WWAN_ON" ]]; then
-  wpa_supplicant -B -i wlex0 -c ${SCRIPT_DIR}/../../wpa.conf
+WWAN_ON=$(ip link | grep ${WWAN_IFACE} | wc -l)
+WPA_ON=$(ps aux | grep wpa | wc -l)
+if [ "$WWAN_ON" -eq 1 ] && [ "$WPA_ON" -eq 1 ]; then
+  wpa_supplicant -B -i ${WWAN_IFACE} -c ${SCRIPT_DIR}/../../wpa.conf
 fi
 
 # create and start docker
 if docker inspect $BUILD_TAG>/dev/null 2>&1; then
-  if docker inspect $CONTAINER >/dev/null 2>&1; then
-    echo "Container Found."
-  else
+  if ! docker inspect $CONTAINER >/dev/null 2>&1; then
     docker create \
       --network none \
       --hostname OpenWRT \
@@ -40,41 +41,50 @@ if docker inspect $BUILD_TAG>/dev/null 2>&1; then
       --volume ${SCRIPT_DIR}/../persistent/etc:/etc \
       --name $CONTAINER $BUILD_TAG >/dev/null
   fi
+  
+  docker start $CONTAINER
 
-  if docker inspect $CONTAINER >/dev/null 2>&1; then
-    docker start $CONTAINER
-
-    # Set network namespace 
-    pid=$(docker inspect -f '{{.State.Pid}}' $CONTAINER)
-    mkdir -p /var/run/netns
-    ln -sf /proc/$pid/ns/net /var/run/netns/$CONTAINER
-  fi
+  # Set network namespace 
+  pid=$(docker inspect -f '{{.State.Pid}}' $CONTAINER)
+  mkdir -p /var/run/netns
+  ln -sf /proc/$pid/ns/net /var/run/netns/$CONTAINER
 else
   echo "No Image Found."
+  exit
 fi
 
-# Create MacVLAN and move into Docker
-ip link add eth0 link ${LAN_IFACE} type macvlan mode bridge
+# Connect LAN
+ip link add eth0 link ${LAN_IFACE} type macvlan mode passthru
 ip link set eth0 netns ${CONTAINER}
 
-# Move WAN into Docker
-ip link set ${WAN_IFACE} netns ${CONTAINER}
-ip netns exec ${CONTAINER} ip link set ${WAN_IFACE} name eth1
+# Connect WAN
+ip link add eth1 link ${WAN_IFACE} type macvlan mode passthru
+ip link set eth1 netns ${CONTAINER}
 
-# Move WWAN into Docker
+# Connect WWAN
+usb_modeswitch -K -v 0bda -p 1a2b
 ip link add eth2 link ${WWAN_IFACE} type macvlan mode passthru
 ip link set eth2 netns openwrt-instance
 
-# Move WiFi into Docker
+# Move WLAN
 iw phy ${WLAN_PHY} set netns name ${CONTAINER}
 ip netns exec ${CONTAINER} ip link set ${WLAN_IFACE} name wlan0
 # iw phy ${WWAN_PHY} set netns name ${CONTAINER}
 # ip netns exec ${CONTAINER} ip link set ${WWAN_IFACE} name wlan1
 
 # Set Host Network
-ip link add ${LAN_IFACE}.wan link ${LAN_IFACE} type macvlan mode bridge
-ip link set ${LAN_IFACE}.wan up
-dhcpcd -q ${LAN_IFACE}.wan
+ip link add veth0 type veth peer name veth1
+ip link set veth0 up
+
+ip link set veth1 netns ${CONTAINER}
+ip netns exec ${CONTAINER} ip link set veth1 up
+
+ip addr add 192.168.100.10 dev veth0
+ip route add 192.168.100.1 dev veth0
+ip route add 192.168.100.100 dev enin0
+ip route add default via 192.168.100.1 dev veth0
+ip route del 192.168.100.0/24 dev enin0
+ip route del 192.168.100.1 dev veth0
 
 # Reload FW
 docker exec -i $CONTAINER sh -c '
@@ -85,6 +95,6 @@ docker exec -i $CONTAINER sh -c '
   done
   /sbin/fw3 -q restart'
 
-# Before Exit
+# Wait for exit
 trap "_cleanup" EXIT
 tail --pid=$pid -f /dev/null
